ffed3ffac75eb9d92119223a500e1b7ac5c7a247
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-09-07 16:39:09 +0200

    Added project files

---
+++ upload/admin/language/sk-sk/catalog/subscription_plan.php
@@ -0,0 +1,48 @@
+<?php
+// Heading
+$_['heading_title']          = 'Plán predplatného';
+
+// Text
+$_['text_success']           = 'Úspech: Plán predplatného bol úspešne upravený!';
+$_['text_list']              = 'Zoznam plánov predplatného';
+$_['text_add']               = 'Pridať predplatné';
+$_['text_edit']              = 'Upraviť predplatné';
+$_['text_subscription']      = 'Predplatné';
+$_['text_trial']             = 'Skúšobné obdobie';
+$_['text_day']               = 'Deň';
+$_['text_week']              = 'Týždeň';
+$_['text_semi_month']        = 'Polmesiac';
+$_['text_month']             = 'Mesiac';
+$_['text_year']              = 'Rok';
+
+// Entry
+$_['entry_name']            = 'Názov';
+$_['entry_trial_duration']  = 'Trvanie';
+$_['entry_trial_cycle']     = 'Cyklus';
+$_['entry_trial_frequency'] = 'Frekvencia';
+$_['entry_trial_status']    = 'Stav';
+$_['entry_duration']        = 'Trvania';
+$_['entry_cycle']           = 'Cyklus';
+$_['entry_frequency']       = 'Frekvencia';
+$_['entry_status']          = 'Stav';
+$_['entry_sort_order']      = 'Radenie';
+
+// Column
+$_['column_name']           = 'Názov';
+$_['column_sort_order']     = 'Radenie';
+$_['column_action']         = 'Akcia';
+
+// Help
+$_['help_trial_duration']   = 'Doba trvania skúšobného obdobia je počet platieb, ktoré používateľ vykoná.';
+$_['help_trial_cycle']      = 'Suma plánu predplatného sa vypočíta na základe frekvencie a cyklov.';
+$_['help_trial_frequency']  = 'Ak použijete frekvenciu „týždeň“ a cyklus „2“, používateľ bude fakturovaný každé 2 týždne.';
+$_['help_duration']         = 'Doba trvania je počet platieb, ktoré používateľ vykoná. Nastavte na 0, ak chcete, aby platby pokračovali, kým nebudú zrušené.';
+$_['help_cycle']            = 'Suma plánu predplatného sa vypočíta na základe frekvencie a cyklov.';
+$_['help_frequency']        = 'Ak použijete frekvenciu „týždeň“ a cyklus „2“, používateľ bude fakturovaný každé 2 týždne.';
+
+// Error
+$_['error_warning']         = 'Upozornenie: Pozorne skontrolujte, či sa vo formulári nenachádzajú chyby!';
+$_['error_permission']      = 'Upozornenie: Nemáte oprávnenie upravovať plány predplatného!';
+$_['error_name']            = 'Názov plánu musí byť dlhší ako 3 a kratší ako 255 znakov!';
+$_['error_trial_duration']  = 'Skúšobná doba musí byť dlhšia ako 0!';
+$_['error_product']         = 'Upozornenie: Tento plán nemožno odstrániť, pretože je aktuálne priradený k %s produktom!';
---
+++ upload/catalog/language/sk-sk/account/address.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['heading_title']      = 'Adresár';
+
+// Text
+$_['text_account']       = 'Účet';
+$_['text_address_book']  = 'Záznamy v adresári';
+$_['text_address_add']   = 'Pridať adresu';
+$_['text_address_edit']  = 'Upraviť adresu';
+$_['text_add']           = 'Vaša adresa bola úspešne pridaná';
+$_['text_edit']          = 'Vaša adresa bola úspešne aktualizovaná';
+$_['text_delete']        = 'Vaša adresa bola úspešne vymazaná';
+$_['text_no_results']    = 'Nemáte žiadne adresy vo vašom účte.';
+
+// Entry
+$_['entry_firstname']    = 'Meno';
+$_['entry_lastname']     = 'Priezvisko';
+$_['entry_company']      = 'Spoločnosť';
+$_['entry_address_1']    = 'Adresa 1';
+$_['entry_address_2']    = 'Adresa 2';
+$_['entry_postcode']     = 'PSČ';
+$_['entry_city']         = 'Mesto';
+$_['entry_country']      = 'Krajina';
+$_['entry_zone']         = 'Oblasť';
+$_['entry_default']      = 'Predvolená adresa';
+
+// Error
+$_['error_token']        = 'Upozornenie: Neplatný token adresy!';
+$_['error_subscription'] = 'Upozornenie: Adresa je stále používaná v %s aktívnych predplatných!';
+$_['error_default']      = 'Upozornenie: Je potrebná predvolená adresa!';
+$_['error_delete']       = 'Upozornenie: Musíte mať aspoň jednu adresu!';
+$_['error_firstname']    = 'Meno musí byť dlhší ako 1 znak a kratšia než 32 znakov!';
+$_['error_lastname']     = 'Priezvisko musí byť dlhší ako 1 znak a kratšia než 32 znakov!';
+$_['error_address_1']    = 'Adresa musí byť dlhší ako 3 znaky a kratšia než 128 znakov!';
+$_['error_postcode']     = 'PSČ musí byť dlhší ako 2 znaky a kratšia než 10 znakov!';
+$_['error_city']         = 'Mesto musí byť dlhší ako 2 znaky a kratšia než 128 znakov!';
+$_['error_country']      = 'Vyberte krajinu!';
+$_['error_zone']         = 'Vyberte oblasť!';
+$_['error_custom_field'] = '%s je povinné!';
+$_['error_regex']        = '%s nie je platný vstup!';
